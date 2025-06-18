34ca633f4e5fd850f0f120930a9df7255809e6e0
Ananthavalli <ananthavalli1991@gmail.com>
2025-06-18 22:27:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # Load env
 load_dotenv()
-client = OpenAI(api_key=os.getenv("AIPROXY_TOKEN") or os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("AIPROXY_TOKEN") or os.getenv("OPENAI_API_KEY"),base_url="https://api.aiproxy.io/v1")
 
 # Load npz files
 text_data = np.load("embed_chunks.npz", allow_pickle=True)
